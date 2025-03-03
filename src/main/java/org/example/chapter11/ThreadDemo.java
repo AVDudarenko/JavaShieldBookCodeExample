aed3cd35e13b61c30428361ab22a766934b41a95
aleksandr <aleksandr.dudarenko@gmail.com>
2025-03-03 23:04:40 +0300

    threads

--- src/main/java/org/example/chapter11/ThreadDemo.java
+++ src/main/java/org/example/chapter11/ThreadDemo.java
@@ -2,8 +2,7 @@ package org.example.chapter11;
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        NewThread newThread = new NewThread("Main thread");
-        newThread.thread.start();
+        NewThread newThread = NewThread.createAndStart("Main thread");
         try {
             for (int i = 5; i > 0; i--) {
                 System.out.println("main thread: " + i);
